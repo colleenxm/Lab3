328eefc4aa86b520d7757d6ce5c297fb3143e1d4
Colleen <ctm6@illinois.edu>
2018-09-18 16:50:17 -0500

    Returns a value but value is too high.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,38 @@
+import java.awt.desktop.SystemEventListener;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
     public static void main(String[] unused) {
-        
+     String fullString = urlToString("http://erdani.com/tdpl/hamlet.txt");
+     int wordCount = 0;
+     fullString = fullString.trim();
+     String[] fullStringArray = fullString.split(" ");
+     for(int i = 0; i < fullStringArray.length; i++) {
+         if(fullStringArray[i] == "") {
+             continue;
+         } else {
+             wordCount += 1;
+         }
+     }
+     System.out.println(wordCount);
     }
 }
